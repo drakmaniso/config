05f5ba1b8ba6b75d575baf3c83f247dd49a76bf1
Laurent Moussault <moussault.laurent@gmail.com>
2019-06-18 17:34:08 +0200

    Fix fish config?

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,3 @@
 bind \e\[P delete-char
 
-set PATH $HOME/bin /opt/node-v10.15.3-linux-armv7l/bin /usr/lib/go-1.11/bin/ $PATH
-
-
-# tabtab source for electron-forge package
-# uninstall by removing these lines or running `tabtab uninstall electron-forge`
-[ -f /opt/node-v10.15.3-linux-armv7l/lib/node_modules/electron-forge/node_modules/tabtab/.completions/electron-forge.fish ]; and . /opt/node-v10.15.3-linux-armv7l/lib/node_modules/electron-forge/node_modules/tabtab/.completions/electron-forge.fish
+set PATH $HOME/bin $HOME/.local/go/bin $PATH
