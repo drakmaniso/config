a57fedccebdddbae807af635d86928544ed5ba05
drakmaniso <moussault.laurent@gmail.com>
2019-07-18 13:16:20 +0200

    Improve prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -5,26 +5,27 @@ function fish_prompt --description 'Prints left prompt'
     #set -l glyph        "\u276f"
     #set -l glyph        "\ue0b0"
     set -l glyph        ""
-    set -l glyph_color  (set_color normal; set_color magenta)
+    set -l glyph_color  (set_color normal; set_color blue)
     set -l pwd          (prompt_pwd)
-    set -l pwd_color    (set_color -b magenta; set_color black)
+    set -l pwd_color    (set_color -b blue; set_color black)
 
-    if test (id -u "$USER") -eq 0
-        set glyph "#"
+    if git_is_repo
+        if git_is_staged || git_is_dirty
+            set glyph_color  (set_color normal; set_color yellow)
+            set pwd_color    (set_color -b yellow; set_color black)
+        else 
+            set glyph_color  (set_color normal; set_color green)
+            set pwd_color    (set_color -b green; set_color black)
+        end
     end
 
     if test "$last_status" -ne 0
-        set pwd_color (set_color red)
+        set pwd_color (set_color -b red; set_color black)
+        set glyph_color  (set_color normal; set_color red)
     end
 
-    if git_is_repo
-        if git_is_staged
-            set glyph_color  (set_color normal; set_color green)
-            set pwd_color    (set_color -b green; set_color black)
-        else if git_is_stashed
-            set glyph_color  (set_color normal; set_color yellow)
-            set pwd_color    (set_color -b yellow; set_color black)
-        end
+    if test (id -u "$USER") -eq 0
+        set glyph "#"
     end
 
     printf "$pwd_color$pwd$glyph_color$glyph "
